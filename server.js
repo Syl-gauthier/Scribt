6c911a8084c77e93c4844461f27a66acfafef7fc
Syl-gauthier <sylvain.gauthier.2012@gadz.org>
2017-03-22 16:26:39 +0900

    created a first template with a navbar

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
- 
+
+app.use(express.static('node_modules/bootstrap/dist'));
+app.use(express.static('public'));
+app.set('view engin', 'pug');
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index.pug');
 });
 var port = process.argv[2] || 3000;
 app.listen(port);
